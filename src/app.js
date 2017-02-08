1954080b930135c5cfc5812432ae9f1a535e1617
Braden Fineberg <Braden@Bradens-MacBook-Pro.local>
2017-02-07 20:15:27 -0500

    login component

--- src/app.js
+++ src/app.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp(
+      {
+        apiKey: "AIzaSyDs4dJwBzcGcmh0vVsmHaQ9WVr55YVb1mI",
+        authDomain: "auth-12577.firebaseapp.com",
+        databaseURL: "https://auth-12577.firebaseio.com",
+        storageBucket: "auth-12577.appspot.com",
+        messagingSenderId: "41573426810"
+      });
+  }
+
   render() {
     return (
       <View>
         <Header headerText="Authentication" />
+        <LoginForm />
       </View>
     );
   }
